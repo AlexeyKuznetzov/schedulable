313aa558da5c6c399eeea71c41aae1bb47294526
AlexeyKuznetzov <alexey.kuznetzov@flatstack.com>
2016-06-14 12:55:46 +0300

    Add day_of_month to params

--- lib/schedulable/schedule_support.rb
+++ lib/schedulable/schedule_support.rb
@@ -3,7 +3,7 @@ module Schedulable
   module ScheduleSupport
     
     def self.param_names
-      [:id, :date, :time, :rule, :until, :count, :interval, day: [], day_of_week: [monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: []]]
+      [:id, :date, :time, :rule, :until, :count, :interval, day: [], day_of_week: [monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: []], day_of_month: []]
     end
     
   end
